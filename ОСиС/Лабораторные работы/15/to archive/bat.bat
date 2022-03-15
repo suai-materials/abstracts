93017facf2eca48c3293562cd01463d43968dd2f
user <frostkslo1@yandex.ru>
2022-03-15 16:36:41 +0300

    Лабы ОСиС

--- ОСиС/Лабораторные работы/15/to archive/bat.bat
+++ ОСиС/Лабораторные работы/15/to archive/bat.bat
@@ -1,2 +1,3 @@
 @echo OFF
-FOR %%x IN (0 1 3 5 7 9) DO 7z a -tzip %%x.zip *.pdf -mx=%%x
+FOR %%x IN (0 1 3 5 7 9) DO 7z a -tzip 1_%%x.zip *.pdf -mx=%%x
+FOR %%x IN (0 1 3 5 7 9) DO 7z a -tzip 2_%%x.zip *.xls -mx=%%x
