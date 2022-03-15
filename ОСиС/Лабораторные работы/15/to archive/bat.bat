148b1596c7465676e229b8619932205089c88eec
user <frostkslo1@yandex.ru>
2022-03-15 20:29:20 +0300

    15 лаба сделана

--- ОСиС/Лабораторные работы/15/to archive/bat.bat
+++ ОСиС/Лабораторные работы/15/to archive/bat.bat
@@ -1,3 +1,3 @@
 @echo OFF
-FOR %%x IN (0 1 3 5 7 9) DO 7z a -tzip 1_%%x.zip *.pdf -mx=%%x
-FOR %%x IN (0 1 3 5 7 9) DO 7z a -tzip 2_%%x.zip *.xls -mx=%%x
+FOR %%x IN (0 1 2 3 4 5) DO rar a  1_%%x.rar *.pdf -m%%x
+FOR %%x IN (0 1 2 3 4 5) DO rar a  2_%%x.rar *.xls -m%%x
