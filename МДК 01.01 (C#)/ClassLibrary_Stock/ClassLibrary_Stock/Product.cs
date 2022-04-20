c7ed6c7276de9c77473d1ff86066eb034d77a074
user <frostkslo1@yandex.ru>
2022-04-20 11:42:40 +0300

    AAAAAAAAAAAAAAAA

---
+++ МДК 01.01 (C#)/ClassLibrary_Stock/ClassLibrary_Stock/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibrary_Stock
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Article { get; set; }
+        public uint Price { get; set; }
+        public UnitMessure Unit { get; set; }
+        public float Marge { get; set; }
+        public DateTime DateOfReceipt { get; set; }
+        public double Quantity { get; }
+
+        public Product()
+        {
+            (Name, Article, Price, Unit, Marge, DateOfReceipt, Quantity) = ("Новый товар", "XXX-000-00", 0,
+                UnitMessure.штука, 0, DateTime.Today, 0);
+        }
+
+        public Product(string name = "Новый товар", string article = "XXX-000-00", uint price = 0, UnitMessure unit = UnitMessure.штука, float merge = 0,
+            DateTime date = default, double quantity = 0)
+        {
+            (Name, Article, Price, Unit, Marge, DateOfReceipt, Quantity) = (name, article, price,
+                unit, merge, date, quantity);
+        }
+    }
+}
